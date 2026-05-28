99214eda43f7821cc75d10c6fd82fa5d11998aef
deathbymanatee <rboudrx4@gmail.com>
2026-05-28 15:55:49 -0500

    test 2

--- modules/default.nix
+++ modules/default.nix
@@ -2,30 +2,14 @@ let
   programModules = builtins.mapAttrs (dir: _: ./programs/${dir}) (
     builtins.removeAttrs (builtins.readDir ./programs) [
       "README.md"
-
-      # default modules
-      "pipewire"
     ]
   );
 
   serviceModules = builtins.mapAttrs (dir: _: ./services/${dir}) (
     builtins.removeAttrs (builtins.readDir ./services) [
       "README.md"
-
-      # included by default
-      "amnezia-vpn"
     ]
   );
 
 in
-{
-  default = {
-    # Modules included by default go here
-    imports = [
-      ./programs/pipewire
-      ./services/amnezia-vpn
-    ];
-  };
-}
-// programModules
-// serviceModules
+programModules // serviceModules
